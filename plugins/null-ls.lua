f0faced93634b2281841aec12ac3998e40874b26
Brent Mitchell <brent.mitchell@kochind.com>
2022-12-02 19:06:53 -0600

    fix: lsp

--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -55,6 +55,16 @@ local M = {}
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
+local lsp_formatting = function(bufnr)
+  vim.lsp.buf.format {
+    filter = function(client)
+      -- apply whatever logic you want (in this example, we'll only use null-ls)
+      return client.name == "null-ls"
+    end,
+    bufnr = bufnr,
+  }
+end
+
 M.setup = function()
   null_ls.setup {
     sources = sources,
@@ -65,7 +75,7 @@ M.setup = function()
           group = augroup,
           buffer = bufnr,
           callback = function()
-            vim.lsp.buf.format { bufnr = bufnr }
+            lsp_formatting(bufnr)
           end,
         })
       end
